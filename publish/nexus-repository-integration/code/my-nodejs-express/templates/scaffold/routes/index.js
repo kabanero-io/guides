6c66c616981c91997134b103eedb6df5f4d7aef6
Sue Chaplain <sue_chaplain@uk.ibm.com>
2020-04-09 16:59:04 +0100

    Editorial review of guide
    Copy of https://github.com/kabanero-io/guide-nexus-repository-integration/pull/9
    from old repo.
    
    General editing update: active voice, sentence simplification, etc.
    In order to work with the new collective guides repo, the examples needed
    to be zipped for download as it is no longer possible to clone the directory.
    
    Moving back to /draft as not ready  for 0.9.0
    
    Signed-off-by: Sue Chaplain <sue_chaplain@uk.ibm.com>

--- publish/nexus-repository-integration/code/my-nodejs-express/templates/scaffold/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index', {msg: "Hello Express!!!"});
-});
-
-module.exports = router;
